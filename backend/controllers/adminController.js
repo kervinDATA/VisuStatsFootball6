d4b321faaf63222f18b9cf4bbd4084401e6ee0b0
kervindholah <kervin92310@hotmail.fr>
2025-01-25 13:20:31 +0100

    page admin gestion users - bouton supprimer

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -20,3 +20,30 @@ exports.getGlobalStatistics = async (req, res) => {
     res.status(500).json({ message: 'Erreur lors de la récupération des statistiques.' });
   }
 };
+
+
+// Contrôleur pour récupérer les utilisateurs
+exports.getUsers = async (req, res) => {
+  try {
+    const query = 'SELECT id, email, role FROM dev_app_foot.users';
+    const result = await db.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Erreur lors de la récupération des utilisateurs :', error);
+    res.status(500).json({ message: 'Erreur lors de la récupération des utilisateurs.' });
+  }
+};
+
+
+// Contrôleur pour supprimer un utilisateur
+exports.deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.id; // Récupère l'ID depuis les paramètres de la requête
+    const query = 'DELETE FROM dev_app_foot.users WHERE id = $1'; // Requête SQL
+    await db.query(query, [userId]); // Exécute la suppression
+    res.status(200).json({ message: 'Utilisateur supprimé avec succès.' });
+  } catch (error) {
+    console.error('Erreur lors de la suppression de l’utilisateur :', error);
+    res.status(500).json({ message: 'Erreur lors de la suppression.' });
+  }
+};
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getGlobalStatistics } = require('../controllers/adminController');
+const { getGlobalStatistics, getUsers, deleteUser } = require('../controllers/adminController');
 
 // Route pour récupérer les statistiques globales
 router.get('/statistics', getGlobalStatistics);
+
+// Route pour récupérer la liste des utilisateurs
+router.get('/users', getUsers);
+
+// Route pour supprimer un utilisateur
+router.delete('/users/:id', deleteUser);
 
 module.exports = router;
